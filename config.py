455236c9e4d814118d1ef9366330996bc305fa45
Jeawills <jeawills2@gmail.com>
2021-02-27 15:51:15 -0500

    posting html

---
+++ config.py
@@ -0,0 +1,5 @@
+remote_db_endpoint = 'databootcamp.ct51fgasslz1.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'team_logic'
+remote_db_pwd = 'Dartlings1!'
+remote_db_name = 'team_logic'
