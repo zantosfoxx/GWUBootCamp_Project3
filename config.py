a6f94e831122907a85e0e92fec7e7fc2566e6c96
Nickoranteng <nickoranteng@gmail.com>
2021-03-14 19:55:05 -0400

    index.htm

--- config.py
+++
@@ -1,6 +0,0 @@
-remote_db_endpoint = 'databootcamp.ct51fgasslz1.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_user = 'team_logic'
-remote_db_pwd = 'Dartlings1!'
-remote_db_name = 'team_logic'
-API_key = "wJwp9NFb-QWNy3d1f9_w"
